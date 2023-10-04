d080fda9602a432ea5189ad971f352d8239df009
Rafael Tavares <rafael_ctavares@hotmail.com>
2023-10-04 00:06:51 -0300

    Adding dunder & if

--- hello.py
+++ hello.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-# Este programa imprime Hello World
-print("Hello, World my friend")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+	export LANG=pt_BR
+
+Execução
+	
+	python3 hello.py
+	ou
+	./hellp.py
+"""
+__version__ = "0.0.1"
+__author__ = "Rafael Tavares"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
